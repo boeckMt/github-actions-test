f05128fd6d62b613b8ad65a3e4ae59c65e230446
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-02-09 12:11:12 +0100

    test imput vars env

--- .github/actions/check-tags/index.sh
+++ .github/actions/check-tags/index.sh
@@ -3,25 +3,14 @@
 # Usage
 # chmod +x index.sh or git update-index --chmod=+x index.sh
 
-latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
-reqVersion="require('${{ inputs.packagePath }}').version"
+reqVersion="require('$PACKAGEPATH').version"
 packageVersion=$(node -p $reqVersion)
 
-packageName="${{ inputs.npmPackageName}}"
-latestNpmVerion=$(npm show "$packageName" version)
-
-if [ "${{ inputs.npmPackageVersion }}" ]
-then
-  latestNpmVerion="${{ inputs.npmPackageVersion }}"
-fi
+echo "$latestTag"
+echo "$NPMPACKAGENAME"
+echo "$PACKAGEPATH"
+echo "$packageVersion"
 
 
-if [ "${latestTag:1}" == "$packageVersion" ] && [ "$packageVersion"  != "$latestNpmVerion" ]
-then
-  echo "Tag:${latestTag} == repo:${packageVersion} && repo:${packageVersion} != npm:${latestNpmVerion}"
-  exit 0
-else
-  echo "Tag:${latestTag} ?== repo:${packageVersion} && repo:${packageVersion} ?== npm:${latestNpmVerion}"
-  exit 1
-fi

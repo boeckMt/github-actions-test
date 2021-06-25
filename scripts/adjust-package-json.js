8a052d1cc2d0f2ef0c671b85053d405885cac0c0
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-06-25 17:21:05 +0200

    set libs package script

--- scripts/adjust-package-json.js
+++ scripts/adjust-package-json.js
@@ -1,8 +1,33 @@
 const FS = require('fs');
 const path = './dist/test-lib/package.json';
-
+const path2 = './dist/test-lib2/package.json';
 module.exports = ({ github, context }) => {
   FS.readFile(path, 'utf8', (error, jsonString) => {
+    if (error) {
+      console.log(`Error read file ${path}:`, error);
+      return;
+    }
+    try {
+      if (jsonString) {
+        const jsonObj = JSON.parse(jsonString);
+        jsonObj.scripts = {
+          "testexit": "node --eval \"console.error('ERROR: exit not working with && ??.\\n')\" && exit 0"
+        };
+        const content = jsonObj;
+        FS.writeFile(path, JSON.stringify(content), err => {
+          if (err) {
+            console.log('Error writing file', err);
+          } else {
+            console.log(`Update file ${path}`);
+          }
+        });
+      }
+    } catch (err) {
+      console.log(`Error parsing JSON `, err);
+    }
+  });
+
+  FS.readFile(path2, 'utf8', (error, jsonString) => {
     if (error) {
       console.log(`Error read file ${path}:`, error);
       return;

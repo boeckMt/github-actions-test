95aaffa524ad2f55a102b1211a8aade757313023
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-06-25 15:37:45 +0200

    exit code after &&

--- scripts/adjust-package-json.js
+++ scripts/adjust-package-json.js
@@ -11,7 +11,7 @@ module.exports = ({ github, context }) => {
       if (jsonString) {
         const jsonObj = JSON.parse(jsonString);
         jsonObj.scripts = {
-          "testexit": "exit 1"
+          "testexit": "node --eval \"console.error('ERROR: exit not working with && ??.\\n')\" && exit 1"
         };
         const content = jsonObj;
         FS.writeFile(path, JSON.stringify(content), err => {

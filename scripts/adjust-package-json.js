a360cf6dd4f3b983610138d58b822e368df59715
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-06-25 14:58:39 +0200

    remove source

--- scripts/adjust-package-json.js
+++ scripts/adjust-package-json.js
@@ -11,7 +11,7 @@ module.exports = ({ github, context }) => {
       if (jsonString) {
         const jsonObj = JSON.parse(jsonString);
         jsonObj.scripts = {
-          "testexit": "node --eval \"console.error('ERROR: this should fail the job!!! \\n')\" && source exit 0"
+          "testexit": "node --eval \"console.error('ERROR: this should fail the job!!! \\n')\" && exit 0"
         };
         const content = jsonObj;
         FS.writeFile(path, JSON.stringify(content), err => {

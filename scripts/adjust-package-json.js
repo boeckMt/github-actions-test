5d29a0e36feebe8a547863143f0d4ff05363072d
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-06-25 12:24:02 +0200

    github script Run a separate file

---
+++ scripts/adjust-package-json.js
@@ -0,0 +1,29 @@
+const FS = require('fs');
+const path = './dist/test-lib/package.json';
+
+module.exports = ({ github, context }) => {
+  FS.readFile(path, 'utf8', (error, jsonString) => {
+    if (error) {
+      console.log(`Error read file ${path}:`, error);
+      return;
+    }
+    try {
+      if (jsonString) {
+        const jsonObj = JSON.parse(jsonString);
+        jsonObj.scripts = {
+          "testexit": "node --eval \"console.error('ERROR: this should fail the job!!! \\n')\" && exit 1"
+        };
+        const content = jsonObj;
+        FS.writeFile(path, JSON.stringify(content), err => {
+          if (err) {
+            console.log('Error writing file', err);
+          } else {
+            console.log(`Update file ${path}`);
+          }
+        });
+      }
+    } catch (err) {
+      console.log(`Error parsing JSON `, err);
+    }
+  });
+}

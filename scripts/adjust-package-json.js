e350fa774de73402cefd2d077df5065e75b114f0
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-07-08 13:18:52 +0200

    adjust workflow build all libs and update

--- scripts/adjust-package-json.js
+++ scripts/adjust-package-json.js
@@ -1,7 +1,32 @@
 const FS = require('fs');
 const path = './dist/test-lib/package.json';
 const path2 = './dist/test-lib2/package.json';
+const path3 = './dist/test-lib3/package.json';
 module.exports = ({ github, context }) => {
+  updateJson(path, (jsonObj) => {
+    jsonObj.scripts = {
+      "testexit": "node --eval \"console.log('test script should not exit process.\\n')\""
+    };
+    return jsonObj;
+  });
+
+  updateJson(path2, (jsonObj) => {
+    jsonObj.scripts = {
+      "testexit": "node --eval \"console.error('ERROR: exit not working with && ??.\\n')\" && exit 1"
+    };
+    return jsonObj;
+  });
+
+
+  updateJson(path3, (jsonObj) => {
+    jsonObj.scripts = {
+      "testexit": "node --eval \"console.log('test script should not be executed.\\n')\""
+    };
+    return jsonObj;
+  });
+}
+
+function updateJson(path, cb) {
   FS.readFile(path, 'utf8', (error, jsonString) => {
     if (error) {
       console.log(`Error read file ${path}:`, error);
@@ -10,35 +35,7 @@ module.exports = ({ github, context }) => {
     try {
       if (jsonString) {
         const jsonObj = JSON.parse(jsonString);
-        jsonObj.scripts = {
-          "testexit": "node --eval \"console.log('test script should not exit process.\\n')\""
-        };
-        const content = jsonObj;
-        FS.writeFile(path, JSON.stringify(content), err => {
-          if (err) {
-            console.log('Error writing file', err);
-          } else {
-            console.log(`Update file ${path}`);
-          }
-        });
-      }
-    } catch (err) {
-      console.log(`Error parsing JSON `, err);
-    }
-  });
-
-  FS.readFile(path2, 'utf8', (error, jsonString) => {
-    if (error) {
-      console.log(`Error read file ${path}:`, error);
-      return;
-    }
-    try {
-      if (jsonString) {
-        const jsonObj = JSON.parse(jsonString);
-        jsonObj.scripts = {
-          "testexit": "node --eval \"console.error('ERROR: exit not working with && ??.\\n')\" && exit 1"
-        };
-        const content = jsonObj;
+        const content = cb(jsonObj);
         FS.writeFile(path, JSON.stringify(content), err => {
           if (err) {
             console.log('Error writing file', err);

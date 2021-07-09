1cda0a60fb56535b0382ddb380f4f68b66f0415b
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-07-09 16:10:21 +0200

    add test scripts

---
+++ scripts/run-npm.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const npm = require("npm");
+const args = process.argv.slice(2);
+function runNPM() {
+  return tslib_1.__awaiter(this, void 0, void 0, function* () {
+    const options = {
+      cliArgs: args
+    };
+    const maybeExitCode = yield npm.default(options);
+    if (typeof maybeExitCode === 'number') {
+      process.exit(maybeExitCode);
+    }
+  });
+}
+exports.default = runNPM;
+runNPM()

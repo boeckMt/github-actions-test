5a289fcad7e6b6c5f38f384f816a10d231ef60a5
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-12-15 15:21:09 +0100

    test octokit get commits for pull

---
+++ .github/github-scripts/checkPrHasTag.js
@@ -0,0 +1,32 @@
+module.exports = async ({ github, context, core, exec, require }) => {
+
+  // https://github.com/actions/toolkit/blob/main/packages/github/src/context.ts#L64
+
+  // https://github.com/actions/github-script#welcome-a-first-time-contributor
+  const repo = context.repo();
+  const pullOptions = github.rest.pulls.listCommits({
+    owner: repo.owner,
+    repo: repo.repo,
+    pull_number: context.pull_request.number
+  })
+  const commits = await github.paginate(pullOptions);
+
+  commits.forEach(c => {
+    console.log(c)
+  });
+
+
+  // https://github.com/actions/toolkit/tree/main/packages/exec#args
+  // https://github.com/actions/toolkit/blob/main/packages/exec/src/exec.ts#L44
+  /* const commitLastTag = await exec.getExecOutput(`git rev-list --tags --max-count=1`);
+
+  const tag = await exec.getExecOutput(`git describe --tags ${commitLastTag.stdout}`);
+
+  console.log('commitLastTag: ', commitLastTag.stdout)
+  console.log('tag: ', tag)
+  console.log(context);
+
+
+  core.exportVariable('testtag', tag)
+  return tag; */
+}

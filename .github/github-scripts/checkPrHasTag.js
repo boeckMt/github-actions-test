abc63cbe8494575650c1229ed0bfb8910121dba7
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-12-15 16:12:19 +0100

    get commits

--- .github/github-scripts/checkPrHasTag.js
+++ .github/github-scripts/checkPrHasTag.js
@@ -1,26 +1,38 @@
+/**
+ * github: https://github.com/octokit/rest.js/
+ *
+ * context: https://github.com/actions/toolkit/blob/main/packages/github/src/interfaces.ts
+ * https://github.com/actions/toolkit/blob/main/packages/github/src/context.ts#L64
+ */
 module.exports = async ({ github, context, core, exec, require }) => {
 
-  // https://github.com/actions/toolkit/blob/main/packages/github/src/context.ts#L64
+  //
 
   // https://github.com/actions/github-script#welcome-a-first-time-contributor
-  // https://github.com/actions/toolkit/blob/main/packages/github/src/interfaces.ts
-  const owner = context.payload.repository.owner.login;
-  const repo = context.payload.repository.name;
-  const pull = context.payload.pull_request.number;
+  if (context.payload.repository && context.payload.pull_request) {
+    const owner = context.payload.repository.owner.login;
+    const repo = context.payload.repository.name;
+    const pull = context.payload.pull_request.number;
 
-  console.log(owner, repo, pull);
+    console.log(owner, repo, pull);
 
 
-  const pullOptions = github.rest.pulls.listCommits({
-    owner,
-    repo,
-    pull_number: pull
-  })
-  const commits = await github.paginate(pullOptions);
+    const pullOptions = github.rest.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: pull
+    })
+    const commits = await github.paginate(pullOptions);
 
-  commits.forEach(c => {
-    console.log(c)
-  });
+    for (const commit of commits) {
+      console.log(commit);
+    }
+
+
+  } else {
+
+  }
+
 
 
   // https://github.com/actions/toolkit/tree/main/packages/exec#args
